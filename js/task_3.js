29ccc3420f33f19b89be909d73f2353789d9e894
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-05-23 12:49:14 +0300

    Доделал ДЗ
    Сделал 3 задание.

--- js/task_3.js
+++ js/task_3.js
@@ -12,7 +12,7 @@ const randomIntegerFromInterval = (min, max) => {
       const canProcess = Math.random() > 0.3;
   
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve([transaction.id, delay]);
       } else{
           reject(transaction.id);
     }
@@ -22,7 +22,7 @@ const randomIntegerFromInterval = (min, max) => {
 
   };
   
-  const logSuccess = (id, time) => {
+  const logSuccess = ([id, time]) => {
     console.log(`Transaction ${id} processed in ${time}ms`);
   };
   
